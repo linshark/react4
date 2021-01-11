1d2191a13c50bc0a64e0f1e07e4d2eda8db196a7
andrejstrashun <andrejstrashun@gmail.com>
2021-01-12 00:48:57 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+
+    btnOnClick(event){
+        console.log('Button clicked!', event.target);
+    }
+
+    render(){
+        return(
+            <div style={{backgroundColor: 'red'}} className="test">
+                <h1>App works!</h1>
+                <h3>It's really working!</h3>
+                <button onClick={this.btnOnClick}>{ this.props.children }</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App>Click on me!</App>,
+    document.getElementById('app')
+);
