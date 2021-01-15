02ae3f5b7f281985905f5eb5dd36d3376a33f39c
linshark <53095343+linshark@users.noreply.github.com>
2021-01-15 23:52:21 +0200

    15012021

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import NewComponent from './new';
 
-class App extends Component{
-    constructor(props){
+class App extends Component {
+    constructor(props) {
         super(props);
+
+        this.state = {
+            timer: 0
+        };
     }
-    render(){
-        return(
+
+    componentWillMount() {
+        setInterval(() => {
+            this.setState({ timer: this.state.timer+1 });
+        }, 100);
+    }
+
+    render() {
+        return (
             <div>
                 <h1>App component</h1>
-                <NewComponent text="hello from app" />
+                { this.state.timer < 50 ? <NewComponent text="hello from app" /> : null }
+                <p>{ this.state.timer }</p>
             </div>
         );
     }
--- src/new.js
+++ src/new.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 
-export default class NewComponent extends Component{
-    constructor(props){
+export default class NewComponent extends Component {
+    constructor(props) {
         super(props);
+
+        console.log('Constructor');
     }
-    render(){
-        return(
+
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+    render() {
+        console.log('render');
+
+        return (
             <div style={{backgroundColor: 'red'}}>
                 <h3>New component</h3>
                 <p>{ this.props.text }</p>
